bf5dfb8edc40a4ba204067c0a05668672ee7aa19
KimSoonYo <rlatnsdy125@naver.com>
2022-09-13 20:01:34 +0900

    programmers Lv3_베스트 앨범 best solution 추가

--- programmers/Lv3_베스트엘범/s1.js
+++ programmers/Lv3_베스트엘범/s1.js
@@ -47,3 +47,37 @@ function solution(genres, plays) {
 
   return answer;
 }
+
+
+// best solution by 자바스크립트 코테 강의
+/*
+문제 해결 전략
+1. 같은 장르끼리 묶기
+2. 묶인 노래들을 재생 순으로 정렬
+3. 노래를 2개까지 자르는 작업
+*/
+
+// 해시 테이블 사용 + 고차 함수
+function solution(genres, plays) {
+  var answer = [];
+  const genreMap = new Map();
+
+  genres
+  .map((genre, index) => [genre, plays[index]])
+  .forEach(([genre, play], index) => {
+    const data = genreMap.get(genre) || { total: 0, songs: [] } // 기본값 설정
+    
+    genreMap.set(genre, {
+      total: data.total + play,
+      songs: [...data.songs, {play, index}].sort((a, b) => b.play - a.play).slice(0, 2)
+    })
+  })
+
+  return [...genreMap.entries()]
+    .sort((a, b) => b[1].total - a[1].total) 
+    .flatMap(item => item[1].songs)   // flatMap(): 전체 배열에 대해 Map함수 실행 후 일차원 배열로 평탄화
+    .map(song => song.index)  
+}
+
+
+
